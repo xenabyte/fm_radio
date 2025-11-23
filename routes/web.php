02318a9a2fc1a74d8cd79bfe6813e06b823856b6
KoderiaNg <iconcept573@gmail.com>
2025-11-23 23:39:06 +0100

    fixing https issue

--- routes/web.php
+++ routes/web.php
@@ -8,18 +8,36 @@ Route::get('/', function () {
 
 
 Route::get('/radio-proxy', function () {
-    $streamUrl = 'http://176.58.96.67:8019/stream';
+   $streamUrl = 'http://176.58.96.67:8019/stream';
 
-    $stream = fopen($streamUrl, 'r');
+    // 1. Open a connection to the insecure stream
+    // We use 'rb' for read binary
+    $stream = @fopen($streamUrl, 'rb');
 
-    return response()->stream(function () use ($stream) {
-        fpassthru($stream);
-        if (connection_aborted()) {
-            fclose($stream);
+    if (!$stream) {
+        abort(502, 'Cannot connect to radio stream.');
+    }
+
+    // 2. Return a Streamed Response
+    // This passes the audio data through to the user chunk by chunk
+    return Response::stream(function () use ($stream) {
+        while (!feof($stream)) {
+            // Read 1024 bytes at a time and output them immediately
+            echo fread($stream, 1024);
+            
+            // Flush the buffer to the browser so audio plays instantly
+            if (ob_get_level() > 0) ob_flush();
+            flush();
+
+            // Stop the loop if the user closes the browser
+            if (connection_aborted()) {
+                break;
+            }
         }
+        fclose($stream);
     }, 200, [
         'Content-Type' => 'audio/mpeg',
         'Cache-Control' => 'no-cache',
-        'Transfer-Encoding' => 'chunked', // Optional, depending on player
+        'X-Accel-Buffering' => 'no', // Tells Nginx not to buffer this
     ]);
 });
